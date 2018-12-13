1234218c96e765e1e7e9d6f637b7fa7fec2b4ac5
medamineabbes <medamineabbes2@gmail.com>
2018-12-13 19:05:31 +0100

    path

--- path.py
+++ path.py
@@ -1,9 +1,14 @@
 import pygame
-def puis(a):
-    b=-1
-    for i in range(a):
-        b*=b
-    return b;
+def distance(a,b,c,d):
+     return math.sqrt((a-b)*(a-b)+(c-d)*(c-d))
+red=(150,0,0)
+green=(0,150,0)
+t0=[5]
+t1=[5]
+ln0=[]
+ln1=[]
+xf=0
+yf=0
 pygame.init()
 Surface=pygame.display.set_mode((300,300))
 Surface.fill((255,255,255))
@@ -17,20 +22,25 @@ while run:
     for e in pygame.event.get():
         if e.type==pygame.QUIT:
             run=False
-        if e.type == pygame.MOUSEBUTTONDOWN and t:
+        if e.type == pygame.MOUSEBUTTONDOWN and t:    
             (mx,my)=pygame.mouse.get_pos()
             mx=mx-mx%10
             my=my-my%10
             i+=1
             if i==1:
-                xd=mx
-                yd=my
-                color=(200,0,0)
-            else :
-                color=(0,0,200)
+                xf=mx
+                yf=my
+                pygame.draw.rect(Surface,(255,0,0),[mx,my,10,10],0)
+            elif i==2 :
+                pygame.draw.rect(Surface,(0,0,255),[mx,my,10,10],0)
                 xa=mx
                 ya=my
-            if i==2:
+            else:
+                t0.append(mx)
+                t1.append(my)
+                pygame.draw.rect(Surface,(0,0,0),[mx,my,10,10],0)
+        if e.type==pygame.KEYDOWN:
+            keys= pygame.key.get_pressed()
+            if keys[pygame.K_SPACE]:
                 t=False
-            pygame.draw.rect(Surface,color,[mx,my,10,10],0)
     pygame.display.update()
